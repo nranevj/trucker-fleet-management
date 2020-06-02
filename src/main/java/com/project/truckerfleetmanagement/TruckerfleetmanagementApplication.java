6b6026a40ed590fcab3ce25c1211d327770215f0
Nitish Rane <rane.nitish@gmail.com>
2020-06-02 10:44:23 -0700

    Changed the Spring Boot files

--- src/main/java/com/project/truckerfleetmanagement/TruckerfleetmanagementApplication.java
+++ src/main/java/com/project/truckerfleetmanagement/TruckerfleetmanagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TruckerfleetmanagementApplication {
+public class TruckerFleetManagementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TruckerfleetmanagementApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TruckerFleetManagementApplication.class, args);
+	}
 
 }
--- src/test/java/com/project/truckerfleetmanagement/TruckerfleetmanagementApplicationTests.java
+++ src/test/java/com/project/truckerfleetmanagement/TruckerfleetmanagementApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TruckerfleetmanagementApplicationTests {
+class TruckerFleetManagementApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
