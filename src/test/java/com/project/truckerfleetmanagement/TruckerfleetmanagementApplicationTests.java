8d74ab350058f2ce1aa10d2f51abc68f6d664448
Nitish Rane <rane.nitish@gmail.com>
2020-06-02 10:40:23 -0700

    Add starter Spring Boot files

---
+++ src/test/java/com/project/truckerfleetmanagement/TruckerfleetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.truckerfleetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckerfleetmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
